51366b023c1836330fc457c88a41f55b4b88d586
Andy King <andy@andyking.online>
2018-07-07 14:14:59 +0100

    Implement slider for bear stats
    Included css for "stats long card" on right.

--- js/honey.js
+++ js/honey.js
@@ -1,3 +1,4 @@
+
 $("#beargen2").on("click", function(){
     var descNum = Math.floor(Math.random() * 6);  
     var speciesNum = Math.floor(Math.random() * 6);
@@ -5,10 +6,12 @@ $("#beargen2").on("click", function(){
     var hatNum = Math.floor(Math.random() * 8);
    
     var arrayDescTitle = ['The Rookie', 'The Washed-Up', 'The Retiree', 'The Unstable', 'The Slick', 'The Incompetent']
+    var arrayDescText = ['The Rookie', 'The Washed-Up', 'The Retiree', 'The Unstable', 'The Slick', 'The Incompetent']
     var descTitle = arrayDescTitle[descNum]; 
-    
+    var descText = arrayDescText[descNum];
     document.getElementById("bear-desc-title").innerHTML = descTitle;
-    
+    document.getElementById("bear-desc-text").innerHTML = descText;
+
     var arraySpeciesTitle = ['Grizzly Bear', 'Polar Bear', 'Giant Panda', 'Black Bear', 'Sun Bear', 'Honey Badger']
     var speciesTitle = arraySpeciesTitle[speciesNum];
 
@@ -19,6 +22,28 @@ $("#beargen2").on("click", function(){
 
     document.getElementById("bear-role-title").innerHTML = roleTitle;
     
-    document.getElementById("bear-deck").style.display = "flex";
+    var arrayHatTitle = ['A Trilby', 'A Top Hat', 'A Bowler', 'A Flat-Cap', 'A Cowboy Hat', 'A Fez', 'A Crown', 'Two Hats!']
+    var hatTitle = arrayHatTitle[hatNum];
+    
+    if (hatNum == 8) {
+        var hatOneNum = Math.floor(Math.random() * 7);
+        var hatTwoNum = Math.floor(Math.random() * 7);
+
+        var hatOneTitle = arrayHatTitle[hatOneNum];
+        var hatTwoTitle = arrayHatTitle[hatTwoNum];        
+    }
+
+    document.getElementById("bear-hat-title").innerHTML = hatTitle;
+
+    document.getElementById("bear-collection").style.display = "block";
+
+});
+
+
+$("#statsSlider").slider();
+$("#statsSlider").on("slide", function(slideEvt) {
+    $("#statssliderBearVal").text(slideEvt.value);
+    var crimStat = 6 - slideEvt.value;
+    $("#statssliderCrimVal").text(crimStat);
 
 });
