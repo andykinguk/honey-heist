5be51c1d63fcc03882834802220798b2a455730d
Andy King <andy@andyking.online>
2018-10-05 15:19:36 +0100

    Decades old commit
    Playing with this project again. Committing whatever changes I had to allow me to rollback when I break something. Looks like just images / js changes to let it load

--- js/honey.js
+++ js/honey.js
@@ -7,15 +7,22 @@ $("#beargen2").on("click", function(){
    
     var arrayDescTitle = ['The Rookie', 'The Washed-Up', 'The Retiree', 'The Unstable', 'The Slick', 'The Incompetent']
     var arrayDescText = ['The Rookie', 'The Washed-Up', 'The Retiree', 'The Unstable', 'The Slick', 'The Incompetent']
+    var arrayDescImg = ['The Rookie', 'The Washed-Up', 'The Retiree', 'The Unstable', 'The Slick', 'The Incompetent']
     var descTitle = arrayDescTitle[descNum]; 
     var descText = arrayDescText[descNum];
     document.getElementById("bear-desc-title").innerHTML = descTitle;
     document.getElementById("bear-desc-text").innerHTML = descText;
 
     var arraySpeciesTitle = ['Grizzly Bear', 'Polar Bear', 'Giant Panda', 'Black Bear', 'Sun Bear', 'Honey Badger']
+    var arraySpeciesText = ['Grizzly Bear', 'Polar Bear', 'Giant Panda', 'Black Bear', 'Sun Bear', 'Honey Badger']
+    var arraySpeciesImg = ['grizzly-bear', 'polar-bear', 'panda-bear2', 'black-bear', 'sun-bear', 'honey-badger']
     var speciesTitle = arraySpeciesTitle[speciesNum];
-
+    var speciesText = arraySpeciesText[speciesNum];
+    var speciesImg = 'img/' + arraySpeciesImg[speciesNum] + '.jpg';
     document.getElementById("bear-species-title").innerHTML = speciesTitle;
+    document.getElementById("bear-species-text").innerHTML = speciesText;
+    document.getElementById("bear-species-img").src = speciesImg;
+
 
     var arrayRoleTitle = ['The Muscle', 'The Brains', 'The Driver', 'The Hacker', 'The Thief', 'The Face']
     var roleTitle = arrayRoleTitle[roleNum];
@@ -23,17 +30,26 @@ $("#beargen2").on("click", function(){
     document.getElementById("bear-role-title").innerHTML = roleTitle;
     
     var arrayHatTitle = ['A Trilby', 'A Top Hat', 'A Bowler', 'A Flat-Cap', 'A Cowboy Hat', 'A Fez', 'A Crown', 'Two Hats!']
+    var arrayHatText = ['A Trilby', 'A Top Hat', 'A Bowler', 'A Flat-Cap', 'A Cowboy Hat', 'A Fez', 'A Crown', 'Two Hats!']
+    var arrayHatImg = ['A Trilby', 'A Top Hat', 'A Bowler', 'A Flat-Cap', 'A Cowboy Hat', 'A Fez', 'A Crown', 'Two Hats!']
     var hatTitle = arrayHatTitle[hatNum];
-    
-    if (hatNum == 8) {
+    var hatText = arrayHatText[hatNum];
+    var hatImg = arrayHatImg[hatNum];
+
+    if (hatNum == 7) {
         var hatOneNum = Math.floor(Math.random() * 7);
         var hatTwoNum = Math.floor(Math.random() * 7);
 
         var hatOneTitle = arrayHatTitle[hatOneNum];
-        var hatTwoTitle = arrayHatTitle[hatTwoNum];        
+        var hatTwoTitle = arrayHatTitle[hatTwoNum];
+        
+        hatText = '\"Give me six hours to chop down a tree and I will send my first four collecting my hats\" - Abraham Lincoln, immediately before picking up his ' + hatOneTitle + ' and ' + hatTwoTitle + '.'
     }
 
     document.getElementById("bear-hat-title").innerHTML = hatTitle;
+    document.getElementById("bear-hat-text").innerHTML = hatText;
+    document.getElementById("bear-hat-img").src = hatTitle;
+
 
     document.getElementById("bear-collection").style.display = "block";
 
